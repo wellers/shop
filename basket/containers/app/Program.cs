0ed70c24170270296cea2f3a53d4a08a1d835666
wellers <paulwelbourne@gmail.com>
2024-04-15 21:20:29 +0100

    Fix formatting.

--- basket/containers/app/Program.cs
+++ basket/containers/app/Program.cs
@@ -14,34 +14,34 @@ var db = redis.GetDatabase();
 
 app.MapGet("/add", async (Guid basketId, int movieId) =>
 {
-    var basket = await db.StringGetAsync(basketId.ToString());
+	var basket = await db.StringGetAsync(basketId.ToString());
 
-    List<int> movies = [];
-    if (basket.HasValue)
-    {
-        try
-        {
-            movies = JsonConvert.DeserializeObject<List<int>>(basket.ToString());
-        }
-        catch 
-        { 
-            return new { Success = false, Message = "Failed to parse basket from cache." };
-        }        
-    }
+	List<int> movies = [];
+	if (basket.HasValue)
+	{
+		try
+		{
+			movies = JsonConvert.DeserializeObject<List<int>>(basket.ToString());
+		}
+		catch
+		{
+			return new { Success = false, Message = "Failed to parse basket from cache." };
+		}
+	}
 
 	movies.Add(movieId);
 
-    var success = await db.StringSetAsync(basketId.ToString(), JsonConvert.SerializeObject(movies));
+	var success = await db.StringSetAsync(basketId.ToString(), JsonConvert.SerializeObject(movies));
 
-    var message = success 
-        ? $"{movies.Count} item(s) added to basket." 
-        : "Failed to add item to basket.";
+	var message = success
+		? $"{movies.Count} item(s) added to basket."
+		: "Failed to add item to basket.";
 
 	return new { Success = success, Message = message };
 });
 
 app.MapGet("/purchase", async (Guid basketId) =>
-{	
+{
 	var basket = await db.StringGetAsync(basketId.ToString());
 
 	List<int> movies = [];
@@ -58,11 +58,11 @@ app.MapGet("/purchase", async (Guid basketId) =>
 	}
 
 	var message = new { BasketId = basketId, Movies = movies };
-	
-    var rabbitMQService = app.Services.GetRequiredService<MessageQueueService>();
+
+	var rabbitMQService = app.Services.GetRequiredService<MessageQueueService>();
 	rabbitMQService.Publish(JsonConvert.SerializeObject(message));
-	
-    return new { Success = true, Message = $"Basket '{basketId}' purchased." };
+
+	return new { Success = true, Message = $"Basket '{basketId}' purchased." };
 });
 
 app.MapGet("/status", () => Results.Json(new { start = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds() }));
